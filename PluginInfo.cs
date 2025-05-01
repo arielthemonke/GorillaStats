e655fd3db28ae37244dd0d5d7401cc33d7eac342
arielthemonke <arielsaks1@gmail.com>
2025-05-01 16:09:39 +0300

    Add files via upload

--- PluginInfo.cs
+++ PluginInfo.cs
@@ -6,6 +6,6 @@
     {
         public const string GUID = "arielthemonke.gorillastats";
         public const string ProjectName = "GorillaStats";
-        public const string Version = "1.0.1";
+        public const string Version = "1.0.2";
     }
 }
